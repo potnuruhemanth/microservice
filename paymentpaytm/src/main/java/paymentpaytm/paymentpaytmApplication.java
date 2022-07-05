8afeae73a35e8201dc2e10c168dd3b03feff9305
potnuruhemanth <108750385+potnuruhemanth@users.noreply.github.com>
2022-07-05 22:35:17 +0530

    Add files via upload

---
+++ paymentpaytm/src/main/java/paymentpaytm/paymentpaytmApplication.java
@@ -0,0 +1,13 @@
+package paymentpaytm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class paymentpaytmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(paymentpaytmApplication.class, args);
+	}
+
+}
